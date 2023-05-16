c4b01ae3f0b7197c8c807e981413c2c7558e0aa6
DesiRorie <desirorie@icloud.com>
2023-05-15 23:03:15 -0400

    .

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,9 +10,11 @@ const Homepage = ({ posts, setPosts }) => {
   const [savedPosts, setSavedPosts] = useState([]);
   const userName = useContext(UserContext);
   useEffect(() => {
-    Axios.get(`${process.env.REACT_APP_BACKEND_URL}/posts`).then((res) => {
-      setSavedPosts(res.data);
-    });
+    Axios.get(`https://ventsocialback-production.up.railway.app/posts`).then(
+      (res) => {
+        setSavedPosts(res.data);
+      }
+    );
   }, []);
 
   return (
