18187b5279ac38f4ad139ebf8e0810c9cc741bb0
DesiRorie <desirorie@icloud.com>
2023-05-16 15:33:27 -0400

    .

--- src/pages/Explorepage.jsx
+++ src/pages/Explorepage.jsx
@@ -1,3 +1,59 @@
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import Feed from "./Feed";
+// import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+
+// const Explorepage = () => {
+//   const handleScroll = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   };
+//   const [query, setQuery] = useState("");
+//   const [memeImages, setMemeImages] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.imgflip.com/get_memes")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         return setMemeImages(data.data.memes);
+//       });
+//   }, []);
+//   const handleQuery = (e) => {
+//     setQuery(e.target.value);
+//   };
+//   return (
+//     <div className="explorePage">
+//       <div className="searchContainer">
+//         <input onChange={handleQuery} placeholder="search" />
+//         <div className="recommended">
+//           <button>Food</button>
+//           <button>Anime</button>
+//           <button>Sports</button>
+//           <button>DIY</button>
+//         </div>
+//       </div>
+//       {memeImages.map((image, id) => {
+//         return (
+//           <div key={id} className="feedImages">
+//             <div>
+//               <img src={image.url} alt={image.name} />
+//             </div>
+//           </div>
+//         );
+//       })}
+//       <div style={{ marginBottom: "5em", textAlign: "center" }}>
+//         <h2>No more posts</h2>
+//         <button className="scrollButton" onClick={handleScroll}>
+//           <ArrowUpwardIcon />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Explorepage;
+
 import React, { useState } from "react";
 import { useEffect } from "react";
 import Feed from "./Feed";
@@ -10,19 +66,34 @@ const Explorepage = () => {
       behavior: "smooth",
     });
   };
+  const [query, setQuery] = useState("");
   const [memeImages, setMemeImages] = useState([]);
+  const [filteredMemeImages, setFilteredMemeImages] = useState([]);
+
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        return setMemeImages(data.data.memes);
+        setMemeImages(data.data.memes);
+        setFilteredMemeImages(data.data.memes);
       });
   }, []);
+
+  const handleQuery = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    const filteredImages = memeImages.filter((image) => {
+      return image.name.toLowerCase().includes(value.toLowerCase());
+    });
+
+    setFilteredMemeImages(filteredImages);
+  };
+
   return (
     <div className="explorePage">
       <div className="searchContainer">
-        <input placeholder="search" />
+        <input onChange={handleQuery} placeholder="search" />
         <div className="recommended">
           <button>Food</button>
           <button>Anime</button>
@@ -30,7 +101,7 @@ const Explorepage = () => {
           <button>DIY</button>
         </div>
       </div>
-      {memeImages.map((image, id) => {
+      {filteredMemeImages.map((image, id) => {
         return (
           <div key={id} className="feedImages">
             <div>
@@ -50,5 +121,3 @@ const Explorepage = () => {
 };
 
 export default Explorepage;
-
-// give the recommendations real values
